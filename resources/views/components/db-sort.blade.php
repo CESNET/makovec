21dfce528d7cffd9579647d81abf4aa6762c04e2
Jan Oppolzer <jan@oppolzer.cz>
2024-08-06 10:57:59 +0200

    Fix sorting when a device type is selected

--- resources/views/components/db-sort.blade.php
+++ resources/views/components/db-sort.blade.php
@@ -1,8 +1,8 @@
 <span class="sm:inline-block block">
-    <a href="?search={{ $search }}&sort={{ $sort }}">
+    <a href="?search={{ $search }}&type={{ $type }}&sort={{ $sort }}">
         <x-hi-chevron-up />
     </a>
-    <a href="?search={{ $search }}&sort={{ $sort }}&order=desc">
+    <a href="?search={{ $search }}&type={{ $type }}&sort={{ $sort }}&order=desc">
         <x-hi-chevron-down />
     </a>
 </span>
